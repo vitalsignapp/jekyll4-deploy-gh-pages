c435db55b0b7bfd620069986deef206c382d077c
Apirak Panatkool <apirakb@gmail.com>
2020-04-22 06:39:37 +0700

    production to development for more log

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ bundle install --jobs 4 --retry 3
 
 echo "Building Jekyll site..."
 
-JEKYLL_ENV=production bundle exec jekyll build
+JEKYLL_ENV=development bundle exec jekyll build
 
 echo "Publishing..."
 
