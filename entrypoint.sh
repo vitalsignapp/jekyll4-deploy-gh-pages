712e03460ebb922e2949ce053fafce9c89397ef0
Apirak Panatkool <apirakb@gmail.com>
2020-04-22 09:54:11 +0700

    move back to _site

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DEST="${JEKYLL_DESTINATION:-docs}"
+DEST="${JEKYLL_DESTINATION:-_site}"
 REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 BRANCH="gh-pages"
 
